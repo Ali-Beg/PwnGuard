369ca901c694f841d8cbaa51e2cf239ad23e6c12
ALI BEG <mbeg937@gmail.com>
2024-12-13 07:09:43 +0530

    Initial commit

--- check_my_passwoed.py
+++ check_my_passwoed.py
@@ -79,7 +79,7 @@ def load_key():
         # For Streamlit Cloud deployment
         if 'streamlit' in sys.modules:
             try:
-                return st.secrets["fernet_key"].encode()
+                return st.secrets["fernet_key"]
             except KeyError:
                 st.error("Streamlit secrets not configured properly")
                 st.stop()
